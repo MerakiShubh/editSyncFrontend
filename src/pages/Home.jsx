bb5347d720447af14c6052b39f0d75ed1ad7bb6a
MerakiShubh <shubhamt.champ@gmail.com>
2024-07-19 12:30:23 +0530

    Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+  const navigate = useNavigate();
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success("Created a new room");
+  };
+
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error("RoomId and username is required");
+      return;
+    }
+    //Redirect
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  };
+
+  const handleInputEnter = (e) => {
+    if (e.code === "Enter") {
+      joinRoom();
+    }
+  };
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img src="/code-sync.png" alt="" className="homePageLogo" />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input
+            value={roomId}
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            onKeyUp={handleInputEnter}
+          />
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            onKeyUp={handleInputEnter}
+          />
+          <button className="btn joinBtn" onClick={joinRoom}>
+            Join
+          </button>
+          <span className="createInfo">
+            If you don&apos;t have and invite then create &nbsp;
+            <a onClick={createNewRoom} href="" className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        Built with 💛&nbsp;by <a href="https://github.com/merakiShubh">Shubh</a>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
