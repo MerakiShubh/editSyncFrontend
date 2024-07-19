00c1ef28ad1fb449790fdf6f7e98a8fa8067a3ed
MerakiShubh <shubhamt.champ@gmail.com>
2024-07-19 12:54:08 +0530

    add prop type

---
+++ src/components/Editor.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import Codemirror from "codemirror";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/theme/dracula.css";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/lib/codemirror.css";
+
+const Editor = ({ socketRef, roomId, onCodeChange }) => {
+  const editorRef = useRef(null);
+  useEffect(() => {
+    async function init() {
+      editorRef.current = Codemirror.fromTextArea(
+        document.getElementById("realtimeEditor"),
+        {
+          mode: { name: "javascript", json: true },
+          theme: "dracula",
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineNumbers: true,
+        }
+      );
+      editorRef.current.on("change", (instance, changes) => {
+        // console.log(changes);
+        const { origin } = changes;
+        const code = instance.getValue();
+        onCodeChange(code);
+        if (origin !== "setValue") {
+          socketRef.current.emit("code-change", {
+            roomId,
+            code,
+          });
+        }
+      });
+    }
+
+    init();
+  }, []);
+
+  useEffect(() => {
+    if (socketRef.current) {
+      socketRef.current.on("code-change", ({ code }) => {
+        if (code !== null) {
+          editorRef.current.setValue(code);
+        }
+      });
+    }
+    return () => {
+      socketRef.current.off("code-change");
+    };
+  }, [socketRef.current]);
+
+  return <textarea id="realtimeEditor"></textarea>;
+};
+
+Editor.propTypes = {
+  socketRef: PropTypes.object.isRequired,
+  roomId: PropTypes.string.isRequired,
+  onCodeChange: PropTypes.func.isRequired,
+};
+
+export default Editor;
