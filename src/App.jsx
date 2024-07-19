9b18253d72673e43047d60b8ef9369bf66f6c6ac
MerakiShubh <shubhamt.champ@gmail.com>
2024-07-19 12:28:18 +0530

    Toaster setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
-
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/editor/:roomId" element={<EditorPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          success: {
+            theme: {
+              primary: "#FF7F50",
+            },
+          },
+        }}
+      >
+        {" "}
+      </Toaster>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor/:roomId" element={<EditorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
